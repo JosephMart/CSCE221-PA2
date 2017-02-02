d2191e808c396903f3a4c6bbbcd16eab59670e79
Gabriel <XyPhoGR@gmail.com>
2017-02-02 12:52:54 -0600

    beginning radix sort
    check int size (in bits)

--- radix-sort.cpp
+++ radix-sort.cpp
@@ -1,17 +1,24 @@
 //============================================================================
 // Name        : radix-sort.cpp
-// Author      : 
-// Date        :
-// Copyright   : 
+// Author      : Gabriel Stella
+// Date        : 2 Feb 2017
+// Copyright   : me only
 // Description : Implementation of radix sort in C++
 //============================================================================
 
+//https://en.wikipedia.org/wiki/Sizeof
+//https://en.wikipedia.org/wiki/C_data_types#limits.h
+//http://www.cplusplus.com/reference/climits/
+
 #include "sort.h"
 #include <iostream>
+#include <climits>
 
 void
 RadixSort::sort(int A[], int size)
 {
+	int bit = 1;
+	int numbits = sizeof(int) * CHAR_BIT;
   /*
      Complete this function with the implementation of radix sort
      algorithm.
