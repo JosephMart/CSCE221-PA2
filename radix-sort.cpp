2e8b13b93091aed95a6b913a7e99e6432877e4df
Gabriel <XyPhoGR@gmail.com>
2017-02-03 17:27:57 -0600

    implemented radix sort

--- radix-sort.cpp
+++ radix-sort.cpp
@@ -13,14 +13,70 @@
 #include "sort.h"
 #include <iostream>
 #include <climits>
+#include <stdexcept>
 
-void
-RadixSort::sort(int A[], int size)
+void RadixSort::sort(int A[], int size)
 {
-	int bit = 1;
+	//LSD radix sort
+
+std::cout << "[";
+		for(int i = 0; i < size; i++) {
+std::cout << A[i] << ' ';
+		}
+std::cout << "]" << std::endl;
+
 	int numbits = sizeof(int) * CHAR_BIT;
-  /*
-     Complete this function with the implementation of radix sort
-     algorithm.
-  */
+	int sectionSize = 8;
+
+	int mask = 0;
+	int possibilities = 1;
+	for(int i = 0; i < sectionSize; i++) {
+		mask = mask << 1;
+		mask++;
+		possibilities *= 2;
+	}
+
+	int rem = numbits % sectionSize;
+	if(rem != 0) {
+		throw std::runtime_error{"Invalid integral size. Don't you use bytes?"};
+	}
+	int sections = numbits / sectionSize;
+
+	int c[possibilities] = {};
+	int b[size];
+	
+	for(int section = 0; section < sections; section++) {
+
+		for(int i = 0; i < possibilities; i++) {
+			c[i] = 0;
+		}
+
+		for(int i = 0; i < size; i++) {
+			int val = A[i];
+			val = (val >> (section * sectionSize)) & mask;
+			c[val]++;
+		}
+
+		for(int i = 1; i < possibilities; i++) {
+			c[i] = c[i] + c[i - 1];
+		}
+
+		for(int i = size - 1; i >= 0; i--) {
+			int val = A[i];
+			int partVal = (val >> (section * sectionSize)) & mask;
+			int index = c[partVal];
+			c[partVal]--;
+			b[index - 1] = val;
+		}
+
+		for(int i = 0; i < size; i++) {
+			A[i] = b[i];
+		}
+	}
+
+std::cout << "[";
+		for(int i = 0; i < size; i++) {
+std::cout << A[i] << ' ';
+		}
+std::cout << "]" << std::endl;
 }
