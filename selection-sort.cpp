e2b23530ac319c678bd7448bbf571b79479b626d
Joseph Martinsen <joseph@martinsen.com>
2017-02-04 15:42:07 -0600

    Revert "Selection Sort Sorting Portion Works"
    This reverts commit 80ef2e5a9d85119923e37fcf7a845ce947d428fa.

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -1,13 +1,10 @@
 //============================================================================
 // Name        : selection-sort.cpp
-// Author      : Joseph Martinsen
+// Author      : 
 // Date        :
-// Copyright   :
+// Copyright   : 
 // Description : Implementation of selection sort in C++
 //============================================================================
-
-// sources used:
-// https://www.youtube.com/watch?v=f8hXR_Hvybo
 
 #include "sort.h"
 #include <iostream>
@@ -15,35 +12,7 @@
 void
 SelectionSort::sort(int A[], int size)				// main entry point
 {
-    /* Complete this function with the implementation of selection sort algorithm
-    Record number of comparisons in variable num_cmps of class Sort
-    */
-    // Used for tracking index of current smallest element
-    int minPos = 0;
-    // Used during swaping
-    int temp = 0;
-
-    for (int i = 0; i < size-1; i++)
-    {
-        // Set current index as min
-        minPos = i;
-
-        // Iterate from i to n looking for the smallest element
-        for (int j = i + 1; j < size; j++)
-        {
-            // Check if a smaller value has been found
-            if (A[j] < A[minPos])
-            {
-                minPos = j;
-            }
-        }
-
-        // Check if a smaller value to swap has been found
-        if (i != minPos)
-        {
-            temp = A[i];
-            A[i] = A[minPos];
-            A[minPos] = temp;
-        }
-    }
+  /* Complete this function with the implementation of selection sort algorithm 
+  Record number of comparisons in variable num_cmps of class Sort
+  */
 }
