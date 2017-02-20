d2ae276bbffe37e244a2a1339f42f1e3f6838127
edo101 <ndozey@gmail.com>
2017-02-19 18:57:33 -0600

    Update insertion-sort.cpp
    finished insertion sort

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -14,4 +14,17 @@ InsertionSort::sort(int A[], int size)				// main entry point
   /* Complete this function with the implementation of insertion sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
   */
+  
+  for (int i = 1; i<sz; i++)
+  {
+	  
+	int tmp = A[i];
+	int j = i;
+	for (; j>0 && tmp < A[j-1]; j--){
+	    A[j] = A[j-1];
+		num_cmps++;
+	}
+	num_cmps++;
+	A[j] = tmp;
+  }
 }
