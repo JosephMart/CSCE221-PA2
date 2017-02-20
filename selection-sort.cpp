307f678a82e58a735e154deb30d56e81ac2b2cba
Joseph Martinsen <joseph@martinsen.com>
2017-02-19 22:48:40 -0600

    Selection Sort WORKS

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -20,13 +20,13 @@ void SelectionSort::sort(int A[], int size)				// main entry point
 	for (int k = 0; k < size-1; k++) {
 		int index = k;
 		for (int i = k+1; i < size; i++) {
-			num_cmps++
+			num_cmps++;
 			if (A[i] < A[index]) {
 				index = i;
 			}
+		}
 		int tmp = A[k];
 		A[k] = A[index];
 		A[index] = tmp;
-		}
     }
 }
